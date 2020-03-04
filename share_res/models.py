988dbf1d5019fbb2fea7a23e425288659d79ff78
SSep <ssep4u@gmail.com>
2020-03-04 18:14:11 +0900

    share_res/models Restaurant

--- share_res/models.py
+++ share_res/models.py
@@ -3,3 +3,11 @@ from django.db import models
 
 class Category(models.Model):
   category_name = models.CharField(max_length=100)
+
+
+class Restaurant(models.Model):
+  category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=3)
+  restaurant_name = models.CharField(max_length=100)
+  restaurant_link = models.CharField(max_length=500)
+  restaurant_content = models.TextField()
+  restaurant_keyword = models.CharField(max_length=50)
