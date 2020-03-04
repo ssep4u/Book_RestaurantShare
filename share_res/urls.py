65236c0595f667bc443b1b727b7575da8a171fbf
SSep <ssep4u@gmail.com>
2020-03-04 18:15:19 +0900

    share_res/urls index, restaurant_detail, create, update, delete; category create, delete

--- share_res/urls.py
+++ share_res/urls.py
@@ -4,7 +4,13 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
-  path('restaurant_detail/', views.restaurant_detail),
-  path('create_restaurant/', views.create_restaurant),
-  path('create_category/', views.create_category),
+  path('restaurant_detail/delete', views.delete_restaurant_delete, name='delete_restaurant_delete'),
+  path('restaurant_detail/<str:res_id>', views.restaurant_detail, name='restaurant_detail_page'),
+  path('restaurant_detail/update_page/update', views.update_restaurant_update, name='update_restaurant_update'),
+  path('restaurant_detail/update_page/<str:res_id>', views.update_restaurant, name='update_restaurant_page'),
+  path('create_restaurant/', views.create_restaurant, name='create_restaurant_page'),
+  path('create_restaurant/create', views.create_restaurant_create, name='create_restaurant_create'),
+  path('create_category/', views.create_category, name='create_category_page'),
+  path('create_category/create', views.create_category_create, name='create_category_create'),
+  path('create_category/delete', views.create_category_delete, name='create_category_delete'),
 ]
