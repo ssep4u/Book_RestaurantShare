b3c70651f6fc8ff175beea6b5a43e5de522ce8ef
SSep <ssep4u@gmail.com>
2020-03-05 10:40:50 +0900

    RestaurantShare/settings send_email/templates, views EmailMessage

--- RestaurantShare/settings.py
+++ RestaurantShare/settings.py
@@ -114,3 +114,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email_settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'gmail주소'
+EMAIL_HOST_PASSWORD = 'gmail패스워드'
--- send_email/views.py
+++ send_email/views.py
@@ -1,8 +1,7 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
+from django.conf import settings
+from django.core.mail import EmailMessage
 from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 from share_res.models import Restaurant
@@ -13,30 +12,13 @@ def send_email(request):
   input_receiver = request.POST['input_receiver']
   input_title = request.POST['input_title']
   input_content = request.POST['input_content']
-  mail_html = '<html><body>'
-  mail_html += '<h1>맛집 공유</h1>'
-  mail_html += '<p>' + input_content + '<br>'
-  mail_html += '발신자님께서 공유하신 맛집은 다음과 같습니다.</p>'
+  restaurants = []
   for checked_res_id in checked_res_list:
-    restaurant = Restaurant.objects.get(id=checked_res_id)
-    mail_html += '<h2>' + restaurant.restaurant_name + '</h2>'
-    mail_html += '<h4>* 관련 링크</h4><p>' + restaurant.restaurant_link + '</p><br>'
-    mail_html += '<h4>* 상세 내용</h4><p>' + restaurant.restaurant_content + '</p><br>'
-    mail_html += '<h4>* 관련 키워드</h4><p>' + restaurant.restaurant_keyword + '</p><br>'
-    mail_html += '<br>'
-  mail_html += '</body></html>'
-
-  # smtp using
-  server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-  server.login('ssep4u@gmail.com', 'gmail패스워드')
-  msg = MIMEMultipart('alternative')
-  msg['Subject'] = input_title
-  msg['From'] = 'ssep4u@gmail.com'
-  msg['To'] = input_receiver
-  mail_html = MIMEText(mail_html, 'html')
-  msg.attach(mail_html)
-  print(msg['To'], type(msg['To']))
-  server.sendmail(msg['From'], msg['To'].split(','), msg.as_string())
-  server.quit()
-
+    restaurants.append(Restaurant.objects.get(id=checked_res_id))
+  content = {'input_content': input_content, 'restaurants': restaurants}
+  msg_html = render_to_string('send_email/email_format.html', content)
+  msg = EmailMessage(subject=input_title, body=msg_html, from_email=settings.EMAIL_HOST_USER,
+                     bcc=input_receiver.split(','))
+  msg.content_subtype = 'html'
+  msg.send()
   return HttpResponseRedirect(reverse('index'))
