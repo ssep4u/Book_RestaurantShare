eecb987096a3dd2cb6a9e60b3632213a80b83c73
SSep <ssep4u@gmail.com>
2020-03-05 10:48:48 +0900

    send_email/views, templates send_success, send_fail

--- send_email/views.py
+++ send_email/views.py
@@ -1,24 +1,26 @@
 from django.conf import settings
 from django.core.mail import EmailMessage
-from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.template.loader import render_to_string
-from django.urls import reverse
 
 from share_res.models import Restaurant
 
 
 def send_email(request):
-  checked_res_list = request.POST.getlist('checks')
-  input_receiver = request.POST['input_receiver']
-  input_title = request.POST['input_title']
-  input_content = request.POST['input_content']
-  restaurants = []
-  for checked_res_id in checked_res_list:
-    restaurants.append(Restaurant.objects.get(id=checked_res_id))
-  content = {'input_content': input_content, 'restaurants': restaurants}
-  msg_html = render_to_string('send_email/email_format.html', content)
-  msg = EmailMessage(subject=input_title, body=msg_html, from_email=settings.EMAIL_HOST_USER,
-                     bcc=input_receiver.split(','))
-  msg.content_subtype = 'html'
-  msg.send()
-  return HttpResponseRedirect(reverse('index'))
+  try:
+    checked_res_list = request.POST.getlist('checks')
+    input_receiver = request.POST['input_receiver']
+    input_title = request.POST['input_title']
+    input_content = request.POST['input_content']
+    restaurants = []
+    for checked_res_id in checked_res_list:
+      restaurants.append(Restaurant.objects.get(id=checked_res_id))
+    content = {'input_content': input_content, 'restaurants': restaurants}
+    msg_html = render_to_string('send_email/email_format.html', content)
+    msg = EmailMessage(subject=input_title, body=msg_html, from_email=settings.EMAIL_HOST_USER,
+                       bcc=input_receiver.split(','))
+    msg.content_subtype = 'html'
+    msg.send()
+    return render(request, 'send_email/send_success.html')
+  except:
+    return render(request, 'send_email/send_fail.html')
